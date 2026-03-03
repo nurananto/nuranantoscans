932e3780a25840ecf54a89a035dbfd51756a0e00
nurananto <34748019+nurananto@users.noreply.github.com>
2026-03-03 12:54:32 +0700

    fix: replace Function constructor anti-debugger with direct debugger statement
    - Function constructor blocked by CSP (unsafe-eval not allowed)
    - Use direct debugger keyword instead (CSP-compatible)
    - setInterval every 1s for continuous pause when DevTools open

--- info-manga.js
+++ info-manga.js
@@ -1460,23 +1460,9 @@ function initProtection() {
     });
 
     // Anti-debugger: pause execution when DevTools is open
-    !function _adb() {
-        try {
-            !function cloop(i) {
-                if (('' + (i / i)).length !== 1 || i % 20 === 0) {
-                    (function() {}).constructor('debugger')();
-                } else {
-                    (function() {}).constructor('debugger')();
-                }
-                cloop(++i);
-            }(0);
-        } catch (e) {}
-    }();
     setInterval(function() {
-        (function() { return false; })
-        ['constructor']('debugger')
-        ['apply']('stateObject');
-    }, 4000);
+        debugger;
+    }, 1000);
     
     dLog('🔒 Protection enabled');
 }
